79223fb0c61c2b291ac51972b1f2dbb86fbce28e
ilabdullin <ilnurabdullin578@gmail.com>
2021-04-09 12:15:47 +0500

    first commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto,
+      blanditiis quo. Odio rerum voluptatum excepturi nam, hic aut. Repellendus
+      dolor animi culpa. Inventore quae nulla totam voluptate fugit quos
+      accusantium.
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto,
+      blanditiis quo. Odio rerum voluptatum excepturi nam, hic aut. Repellendus
+      dolor animi culpa. Inventore quae nulla totam voluptate fugit quos
+      accusantium.
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto,
+      blanditiis quo. Odio rerum voluptatum excepturi nam, hic aut. Repellendus
+      dolor animi culpa. Inventore quae nulla totam voluptate fugit quos
+      accusantium.
+    </p>
+  </q-page>
+</template>
+
+<script>
+export default {};
+</script>
